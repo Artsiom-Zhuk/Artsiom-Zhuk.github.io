898c83a7e712835b2e86616324807da00f94919f
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-01-27 15:37:07 +0300

    #3/feat: add Component PersonItem

--- src/views/person-item.js
+++ src/views/person-item.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const PersonItem = ({ person: { name, titlePhoto, birthDate } }) => (
-  <div>
-    <h2>Страница деятеля</h2>
-    <p>Страница деятеля культуры</p>
-    <p>{name}</p>
-    <img src={`/assets/${titlePhoto}`} alt="" />
-    <p>{birthDate}</p>
-  </div>
-);
+class PersonItem extends Component {
+  componentDidMount() {}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.person.person === null) {
+      this.props.history.push("/not-found");
+    }
+  }
+  render() {
+    const { person } = this.props;
+
+    let personContent;
+    personContent = (
+      <div>
+        <div className="row">
+          <div className="col-md-6">
+            <p>Back to person list</p>
+          </div>
+          <div className="col-md-6" />
+        </div>
+        <h2>Страница деятеля</h2>
+        <p>Страница деятеля культуры</p>
+        <p>{person.name}</p>
+        <img src={`/assets/${person.titlePhoto}`} alt="" />
+        <p>{person.birthDate}</p>
+      </div>
+    );
+    return (
+      <div className="person">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{personContent}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default PersonItem;
