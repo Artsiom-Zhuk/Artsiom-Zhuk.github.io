8e75fcd60c961d357540a9db1ff510f2fe495c7d
Artsiom-Zhuk <artsiom.zhuk498@gmail.com>
2019-01-28 22:27:18 +0300

    adaptation main page for tablet mobile presentation

--- src/views/main-page/index.js
+++ src/views/main-page/index.js
@@ -21,24 +21,25 @@ const MainPage = ({ person: { name, titlePhoto, birthDate, birthPlace, id }, goT
           <div className="main-page__div-about-artist">
             <Media body>
               <Media heading>
-                <div>{t(name)}</div>
+                <div div className="main-page__div-container-name-artist">{t(name)}</div>
               </Media>
               <div>{t('Место рождения')}: {t(birthPlace)}</div>
               <div>{t('Дата рождения')}: {birthDate}</div>
-              <Button className="main-page__button-more" onClick={goToPersonPage} color="primary">{t('Подробнее')}</Button>{' '}
+              <div className="main-page__div-container-button-more">
+               <Button className="main-page__button-more" onClick={goToPersonPage} color="primary">{t('Подробнее')}</Button>{' '}
+               </div>
             </Media>
           </div>
         </Media>
         <div className="main-page__div-container-about-portal">
-            <h4>{t('О портале')}</h4>
-            <div>{t('Портал содержит информацию о жизни и творчестве выдающихся фотографов Беларуси')}</div>
+            <h4 className="main-page__h4-title-about-portal">{t('О портале')}</h4>
+            <div className="main-page__div-information-about-portal">{t('Портал содержит информацию о жизни и творчестве выдающихся фотографов Беларуси')}</div>
         </div>
         <div className="main-page__div-container-team-project">
           <h5 className="main-page__h5-team-ptoject">{t('Команда проекта')}</h5>
           <div className="main-page__div-team-project">
             <div className="main-page__div-developer">
-              <div className="main-page__div-img-first-developer">
-              </div>
+              <div className="main-page__div-img-developer main-page__div-img-first-developer" />
               <div className="main-page__div-name-developer">
                 {t('Владислав')}
               </div>
@@ -47,8 +48,7 @@ const MainPage = ({ person: { name, titlePhoto, birthDate, birthPlace, id }, goT
               </div>
             </div>
             <div className="main-page__div-developer">
-              <div className="main-page__div-img-second-developer">
-              </div>
+              <div className="main-page__div-img-developer main-page__div-img-second-developer" />
               <div className="main-page__div-name-developer">
                 {t('Анатолий')}
               </div>
@@ -57,8 +57,7 @@ const MainPage = ({ person: { name, titlePhoto, birthDate, birthPlace, id }, goT
               </div>
             </div>
             <div className="main-page__div-developer">
-              <div className="main-page__div-img-third-developer">
-              </div>
+              <div className="main-page__div-img-developer main-page__div-img-third-developer"/>
               <div className="main-page__div-name-developer">
                 {t('Павел')}
               </div>
@@ -66,9 +65,8 @@ const MainPage = ({ person: { name, titlePhoto, birthDate, birthPlace, id }, goT
                 <a href="https://github.com/CuppaKe">GitHub</a>
               </div>
             </div>
-            <div className="main-page__div-developer">
-              <div className="main-page__div-img-fourth-developer">
-              </div>
+            <div className="main-page__div-developer main-page__div-last-developer">
+              <div className="main-page__div-img-developer main-page__div-img-fourth-developer"/>
               <div className="main-page__div-name-developer">
                 {t('Артём')}
               </div>
