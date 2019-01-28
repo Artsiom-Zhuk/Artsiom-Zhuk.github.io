83bda2e4d6118d8c253dfddc0de8d74c9f525ee4
CuppaKe <turnman87@gmail.com>
2019-01-28 13:44:14 +0300

    #17/feat: add localization to main page, fix: component: person of the day

--- src/containers/app.js
+++ src/containers/app.js
@@ -32,20 +32,20 @@ class App extends Component {
   }
 
   render() {
+    const { t, i18n } = this.props;
+    const changeLanguage = (lng) => {
+      i18n.changeLanguage(lng);
+    };
     const { pageToShow, persons, personIndex } = this.state;
     const randomIndex = Math.floor(Math.random() * persons.length);
     const app = {
       mainPage: <MainPage
         person={persons[randomIndex]}
         goToPersonPage={() => this.goToPersonPage(randomIndex)}
+        t={t}
       />,
       personList: <PersonList persons={persons} goToPersonPage={this.goToPersonPage} />,
       personItem: <PersonItem person={persons[personIndex]} />,
-    };
-
-    const { t, i18n } = this.props;
-    const changeLanguage = (lng) => {
-      i18n.changeLanguage(lng);
     };
 
     return (
--- src/views/main-page/index.js
+++ src/views/main-page/index.js
@@ -2,47 +2,44 @@ import React from 'react';
 import { Media } from 'reactstrap';
 import { Badge } from 'reactstrap';
 import { Button } from 'reactstrap';
-
-import bazan_3 from '../../assets/bazan_3.jpg';
 import './main-page.css';
 
-const MainPage = ({ person: { name, titlePhoto, birthDate }, goToPersonPage }) => (
+const MainPage = ({ person: { name, titlePhoto, birthDate, birthPlace, id }, goToPersonPage, t }) => (
   <div>
     <div className="main-page__div-background">
       <div className="main-page__div-wrapper">
         <div className="main-page__div-container-title-artist-day">
-          <h1><Badge className="main-page__badge-title-artist-day" color="dark">Фотограф дня</Badge></h1>
+          <h1><Badge className="main-page__badge-title-artist-day" color="dark">{t('Фотограф дня')}</Badge></h1>
         </div>
         <Media className="main-page__media-container-artist-day">
           <div className="main-page__div-container-image-artist">
             <Media left href="#">
-              <Media className="main-page__media-image-artist" object src={bazan_3} alt="Generic placeholder image" />
+              <Media className="main-page__media-image-artist" object src={require(`../../assets/${titlePhoto}`) } alt={id} />
             </Media>
           </div>
           <div className="main-page__div-about-artist">
             <Media body>
               <Media heading>
-                <div>{name}</div>
+                <div>{t(name)}</div>
               </Media>
-              <div>Место рождения: Унеча, Брянск, СССР</div>
-              <div>Дата рождения: {birthDate}</div>
-              <div>Года жизни: 1953 - наши дни</div>
-              <Button className="main-page__button-more" onClick={goToPersonPage} color="primary">Подробнее</Button>{' '}
+              <div>{t('Место рождения')}: {t(birthPlace)}</div>
+              <div>{t('Дата рождения')}: {birthDate}</div>
+              <Button className="main-page__button-more" onClick={goToPersonPage} color="primary">{t('Подробнее')}</Button>{' '}
             </Media>
           </div>
         </Media>
         <div className="main-page__div-container-about-portal">
-            <h4>О портале</h4>
-            <div>Портал содержит информацию о жизни и творчести выдающихся фотогрофов Беларуси.</div>
+            <h4>{t('О портале')}</h4>
+            <div>{t('Портал содержит информацию о жизни и творчестве выдающихся фотографов Беларуси')}</div>
         </div>
         <div className="main-page__div-container-team-project">
-          <h5 className="main-page__h5-team-ptoject">Команда проекта</h5>
+          <h5 className="main-page__h5-team-ptoject">{t('Команда проекта')}</h5>
           <div className="main-page__div-team-project">
             <div className="main-page__div-developer">
               <div className="main-page__div-img-first-developer">
               </div>
               <div className="main-page__div-name-developer">
-                Владислав
+                {t('Владислав')}
               </div>
               <div>
                 <a href="https://github.com/sunrisekovalevski">GitHub</a>
@@ -52,7 +49,7 @@ const MainPage = ({ person: { name, titlePhoto, birthDate }, goToPersonPage }) =
               <div className="main-page__div-img-second-developer">
               </div>
               <div className="main-page__div-name-developer">
-                Анатолий
+                {t('Анатолий')}
               </div>
               <div>
                 <a href="https://github.com/dzhudzhi">GitHub</a>
@@ -62,7 +59,7 @@ const MainPage = ({ person: { name, titlePhoto, birthDate }, goToPersonPage }) =
               <div className="main-page__div-img-third-developer">
               </div>
               <div className="main-page__div-name-developer">
-                Павел
+                {t('Павел')}
               </div>
               <div>
                 <a href="https://github.com/CuppaKe">GitHub</a>
@@ -72,7 +69,7 @@ const MainPage = ({ person: { name, titlePhoto, birthDate }, goToPersonPage }) =
               <div className="main-page__div-img-fourth-developer">
               </div>
               <div className="main-page__div-name-developer">
-                Артём
+                {t('Артём')}
               </div>
               <div>
                 <a href="https://github.com/Artsiom-Zhuk">GitHub</a>
